209de6d18736d561777b2242a399333dae0a9b3d
adam fader <adamfader@gmail.com>
2016-09-12 05:14:01 -0400

    initial site files

---
+++ controllers/portfolio_controller.js
@@ -0,0 +1,89 @@
+/*
+Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+*/
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+//var portfolio = require('../models/portfolio.js');
+
+// redirects user to /burgers
+router.get('/', function (req, res) {
+  res.redirect('/index');
+});
+
+//get request that displays the index.html file
+router.get('/index', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/index.html'));
+});
+
+//get request that displays the portfolio.html file
+router.get('/portfolio', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/portfolio.html'));
+});
+
+//get request that displays the contact.html file
+router.get('/contact', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/contact.html'));
+});
+
+//get request that displays the star trek hangman index.html file
+router.get('/hangman', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/hangman/index.html'));
+});
+
+//get request that displays the star wars rpg game index.html file
+router.get('/rpg', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/rpg/index.html'));
+});
+
+//get request that displays the simpsons trivia game index.html file
+router.get('/simpsons', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/simpsons/index.html'));
+});
+
+//get request that displays the giphy api index.html file
+router.get('/giphy', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/giphy/index.html'));
+});
+
+//get request that displays the trains firebase schedule index.html file
+router.get('/trains', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/trains/index.html'));
+});
+
+//get request that displays the i was here team project index.html file
+router.get('/travel', function(req, res){
+  res.sendFile(path.join(__dirname + './../public/travel/index.html'));
+});
+
+// accesses the all function in burger.js
+// tells the index.handlebars to render and sends select all response as an object
+router.get('/burgers', function (req, res) {
+  burger.all(function (data) {
+    var hbsObject = { burgers: data };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+// accesses the create function in burger.js
+// passes the burger name from the index.handlebars form and passes the db column name
+// redirects to .get /burgers and reloads page
+router.post('/burgers/create', function (req, res) {
+  burger.create(['burger_name'], [req.body.burger_name], function () {
+    res.redirect('/burgers');
+  });
+});
+
+// accesses the update function in burger.js
+// passes burger id and hidden input value from form in index.handlebars
+// redirects to .get /burgers and reloads page
+router.put('/burgers/update/:id', function (req, res) {
+  var condition = 'id = ' + req.params.id;
+  console.log('condition', condition);
+  burger.update({ devoured: req.body.devoured }, condition, function () {
+    res.redirect('/burgers');
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var path = require('path');
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+var exphbs = require('express-handlebars');
+var hbs = exphbs.create({
+  extname:'handlebars',
+  layoutsDir:  './views/layouts',
+  defaultLayout: 'main',
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    prettifyDatetime: function(timestamp) {
+      return moment(timestamp).format('lll');
+    } 
+  }
+
+});
+
+// Initialize engine
+app.engine('handlebars', hbs.engine);
+
+// Set engine
+app.set('view engine', 'handlebars');
+
+//require('./routing/html_routes.js')(app);
+
+// routes requests to portfolio_controller.js
+var routes = require('./controllers/portfolio_controller.js');
+app.use('/', routes);
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+  console.log('App listening on PORT ' + PORT);
+})
