77cf35265e3fd2130cb034c4114a4a4dfa102e5a
adam fader <adamfader@gmail.com>
2016-11-28 02:32:57 -0500

    ADDING SUCCESS MESSAGE AFTER EMAIL SUCCESS

--- controllers/portfolio_controller.js
+++ controllers/portfolio_controller.js
@@ -12,25 +12,22 @@ router.get('/', function (req, res) {
 });
 
 //get request that displays the index.html file
-router.get('/index', function(req, res){
+router.get('/index', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/index.html'));
 });
 
 //get request that displays the portfolio.html file
-router.get('/portfolio', function(req, res){
+router.get('/portfolio', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/portfolio.html'));
 });
 
 //get request that displays the contact.html file
-router.get('/contact', function(req, res){
+router.get('/contact', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/contact.html'));
 });
 
 //handles form submit from contact.html file
-router.post('/contact/submit', function(req, res){
-  // burger.create(['burger_name'], [req.body.burger_name], function () {
-  //   res.redirect('/burgers');
-  // });
+router.post('/contact/submit', function(req, res) {
   var helper = require('sendgrid').mail;
   var from_email = new helper.Email(req.body.email);
   var to_email = new helper.Email('adamfader@gmail.com');
@@ -46,54 +43,43 @@ router.post('/contact/submit', function(req, res){
   });
 
   sg.API(request, function(error, response) {
-    console.log('Response', response.statusCode);
+    console.log(response.statusCode);
     console.log(response.body);
     console.log(response.headers);
     if (response.statusCode == 202) {
-      res.redirect('/contact'); 
+      // res.redirect('/contact');
+      res.send('SUCCESS!')
     }
   });
-
-  // sendgrid.send({
-  //   to: 'adamfader@gmail.com',
-  //   from: [req.body.email],
-  //   subject: [req.body.sender],
-  //   text: [req.body.message]
-  // }, function(err, json) {
-  //   if (err) { return res.send('Error!'); }
-  //   res.send('Success!');
-  // });
-
-  // res.redirect('/contact');
 });
 
 //get request that displays the star trek hangman index.html file
-router.get('/hangman', function(req, res){
+router.get('/hangman', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/hangman/index.html'));
 });
 
 //get request that displays the star wars rpg game index.html file
-router.get('/rpg', function(req, res){
+router.get('/rpg', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/rpg/index.html'));
 });
 
 //get request that displays the simpsons trivia game index.html file
-router.get('/simpsons', function(req, res){
+router.get('/simpsons', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/simpsons/index.html'));
 });
 
 //get request that displays the giphy api index.html file
-router.get('/giphy', function(req, res){
+router.get('/giphy', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/giphy/index.html'));
 });
 
 //get request that displays the trains firebase schedule index.html file
-router.get('/trains', function(req, res){
+router.get('/trains', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/trains/index.html'));
 });
 
 //get request that displays the i was here team project index.html file
-router.get('/travel', function(req, res){
+router.get('/travel', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/travel/index.html'));
 });
 
@@ -128,7 +114,7 @@ router.put('/burgers/update/:id', function (req, res) {
 });
 
 //get request that displays the resume_2016.pdf file
-router.get('/resume', function(req, res){
+router.get('/resume', function(req, res) {
   res.sendFile(path.join(__dirname + './../public/resume/Resume_2016.pdf'));
 });
 
