2537cca8c6a47b4a6a4fab5169c351d1a0159f66
adam fader <adamfader@gmail.com>
2017-03-03 13:11:10 -0500

    changed user page url detection to local and deployed

--- public/travel/assets/javascript/script.js
+++ public/travel/assets/javascript/script.js
@@ -48,7 +48,7 @@ $(document).on('ready', function(){
   }
 
   //check if current page is user.html
-  if(window.location.href === "file:///C:/Users/midwe/Desktop/Bootcamp/Portfolio_Integrated/travel/user.html") {
+  if(window.location.href === "http://localhost:3000/travel/user.html") {
     //check if user is logged in
     if(authData !== null){ //checks to see if client is authenticated
 
